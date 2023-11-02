e0bb8f34525de1d7c20f26b5c6f32db3c62bb75b
wapuz <triwahyuismoyob31@gmail.com>
2023-11-02 14:29:49 +0800

    baru mulai, tes push ke git

---
+++ phpdasar/index.php
@@ -0,0 +1,31 @@
+<?php
+require 'functions.php';
+
+// $buah = query("SELECT * FROM `buah`");
+
+?>
+
+<body>
+    <h1>Tabel Bu-Buahan</h1>
+    <table class="table caption-top">
+    <caption>List of Buah</caption>
+    <thead class="table-dark">
+        <th> No </th>
+        <th> Nama </th>
+        <th> Gambar </th>
+        <th> Deskripsi </th>
+        <th> Aksi </th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>#</td>
+            <td>Edit | Hapus</td>
+        </tr>
+    </tbody>
+    </table>
+    
+</body>
+</html>
